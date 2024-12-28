e32034cd57990f436b9088a0dbb7e305babd99ef
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2024-12-28 16:56:47 +0300

    refactor: improve eslint support and add prettier support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import { dirname } from 'path';
+import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +15,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  eslintConfigPrettier,
+  eslintPluginPrettier,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'arrow-body-style': 'warn',
+      'no-duplicate-imports': ['error', { includeExports: true }],
+      'no-undef': 'warn',
+      'no-unused-vars': 'warn',
+      'require-await': 'warn',
+      'sort-keys': 'warn',
+      'sort-vars': 'warn',
+    },
+  },
 ];
 
 export default eslintConfig;
